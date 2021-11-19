a1c8f9a0ea5f9a09958619945e0120048ef3d4ea
lyndontavares <integraldominio@gmail.com>
2021-11-18 22:48:43 -0300

    *

--- frontsimples/src/app/product.service.spec.ts
+++ frontsimples/src/app/product.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-
 import { ProductService } from './product.service';
 import { ProductData } from './product-data.model';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('Teste do service ProductService', () => {
   let service: ProductService;
@@ -24,10 +24,19 @@ describe('Teste do service ProductService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [
+        HttpClientTestingModule,
+        MatSnackBarModule],
+      providers: [
+        ProductService
+      ]
     });
     service = TestBed.inject(ProductService);
     httpController = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    // Após cada teste, certifique - se de que não há mais solicitações pendentes.
+    httpController.verify();
   });
 
   it('deve ser criado o serviço ProductService', () => {
@@ -39,10 +48,80 @@ describe('Teste do service ProductService', () => {
     service.read().subscribe(result => {
       produtosApi = result;
     });
-    const request = httpController.expectOne(service.baseUrl);
-    request.flush(produtosDummy);
-    expect(request.request.method).toBe('GET');
-    expect(produtosApi).toEqual(produtosDummy);
+    const request = httpController.expectOne(service.baseUrl); // Espera uma requisição HTTP
+    request.flush(produtosDummy); // retorna um array de produtos dummy
+    expect(request.request.method).toBe('GET'); // verifica se o método é GET
+    expect(produtosApi).toEqual(produtosDummy); // verifica se o array retornado é o mesmo que o array dummy
   });
+
+  /*
+  //caso o subscribe retorne erro. No nosso projeto exemplo, não é possível testar isso.
+  it('deve verificar exceção', () => {
+    let error: any;
+    service.read().subscribe({ error: (e) => error = e }); // https://rxjs.dev/deprecations/subscribe-arguments
+    const request = httpController.expectOne(service.baseUrl); // Espera uma requisição HTTP
+    request.flush("Something went wrong", {
+      status: 404,
+      statusText: "Network error"
+    });
+    expect(error.indexOf("Error retrieving travellers data") >= 0).toBeTruthy();
+  })
+  */
+
+  it('deve verificar exceção. Método showMessage é chamado para mostrar mensagem de erro', () => {
+    spyOn(service, "showMessage")
+    service.read().subscribe(null)
+    const request = httpController.expectOne(service.baseUrl);
+    request.flush("Something went wrong", {
+      status: 404,
+      statusText: "Network error"
+    });
+
+    expect(service.showMessage).toHaveBeenCalledTimes(1) // podemos evrtificar se o método foi chamado 1 vez
+    //expect(service.showMessage).toHaveBeenCalled()     // ou seja, se o método foi simplesmente chamado
+  });
+
+  it('deve verificar exclução de produto', () => {
+    const id = '1'
+    service.delete(id).subscribe()
+    const request = httpController.expectOne({
+      method: "DELETE",
+      url: service.baseUrl+`/${id}` // verifica se a url é a mesma que a baseUrl + o id
+    })
+    request.flush('')
+    expect(request.request.method).toBe('DELETE')
+  });
+
+  it('deve verificar atualização de produto', () => {
+    const produto = {
+      id: 1,
+      name: 'Produto 1',
+      price: 100,
+      quantity: 10
+    }
+    service.update(produto).subscribe()
+    const request = httpController.expectOne({
+      method: "PUT",
+      url: service.baseUrl+`/${produto.id}` // verifica se a url é a mesma que a baseUrl + o id
+    })
+    request.flush(produto)
+    expect(request.request.method).toBe('PUT')
+  })
+
+  it('deve verificar criação de produto', () => {
+    const produto = {
+      id: 1,
+      name: 'Produto 1',
+      price: 100,
+      quantity: 10
+    }
+    service.create(produto).subscribe()
+    const request = httpController.expectOne({
+      method: "POST",
+      url: service.baseUrl
+    })
+    request.flush(produto)
+    expect(request.request.method).toBe('POST')
+  })
 
 });
